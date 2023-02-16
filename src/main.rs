2c089221776be00e4363313edcb5d5a59bdf0ee5
Beldur <alexluecking@gmail.com>
2023-02-16 22:29:03 +0100

    ndle concurrent clients

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,44 @@
-use std::io::{Read, Write};
-use std::net::TcpListener;
+use anyhow::Result;
+use bytes::BytesMut;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<()> {
     println!("Logs from your program will appear here!");
 
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:6379").await?;
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
+    loop {
+        let incoming = listener.accept().await;
+
+        match incoming {
+            Ok((stream, _)) => {
                 println!("Accepted new connection");
 
-                let mut buffer = [0; 512];
-
-                loop {
-                    let bytes_read = stream.read(&mut buffer).unwrap();
-
-                    if bytes_read == 0 {
-                        println!("Client closed the connection");
-                        break;
-                    }
-
-                    stream.write("+PONG\r\n".as_bytes()).unwrap();
-                }
+                tokio::spawn(async move {
+                    handle_connection(stream).await.unwrap();
+                });
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+async fn handle_connection(mut stream: TcpStream) -> Result<()> {
+    let mut buffer = [0; 512];
+
+    loop {
+        let bytes_read = stream.read(&mut buffer).await?;
+
+        if bytes_read == 0 {
+            println!("Client closed the connection");
+            break;
+        }
+
+        stream.write("+PONG\r\n".as_bytes()).await?;
+    }
+    Ok(())
 }
