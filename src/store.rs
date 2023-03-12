b1d3430409324f729c71b3da3b827ea72f4c1985
alu <alu@pixum.com>
2023-03-12 21:20:04 +0100

    Expiry

--- src/store.rs
+++ src/store.rs
@@ -1,7 +1,15 @@
-use std::collections::HashMap;
+use std::{
+    collections::HashMap,
+    time::{Duration, Instant},
+};
 
 pub struct Store {
-    data: HashMap<String, String>,
+    data: HashMap<String, Entry>,
+}
+
+struct Entry {
+    value: String,
+    t: Option<Instant>,
 }
 
 impl Store {
@@ -12,10 +20,31 @@ impl Store {
     }
 
     pub fn set(&mut self, key: String, value: String) {
-        self.data.insert(key, value);
+        self.data.insert(key, Entry { value, t: None });
     }
 
-    pub fn get(&self, key: String) -> Option<String> {
-        self.data.get(key.as_str()).cloned()
+    pub fn get(&mut self, key: String) -> Option<String> {
+        match self.data.get(key.as_str()) {
+            Some(entry) => {
+                if let Some(t) = entry.t {
+                    if Instant::now() > t {
+                        self.data.remove(key.as_str());
+                        return None;
+                    }
+                }
+
+                Some(entry.value.clone())
+            }
+            None => None,
+        }
+    }
+
+    pub fn set_with_expiry(&mut self, key: String, value: String, px: u64) {
+        let entry = Entry {
+            value,
+            t: Some(Instant::now() + Duration::from_millis(px)),
+        };
+
+        self.data.insert(key, entry);
     }
 }
